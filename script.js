471b0ccdc257a41a886234cabe9afead4cc1dd1f
Mahmoud Shata <mahmoudhosam23@gmail.com>
2023-03-31 15:18:48 +0200

    make profile and notification

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ $("#toggle-sidebar").click(function () {
   $("#small-sidebar").toggleClass("hide");
   $("#sidebar").toggleClass("hide");
 });
+$("#buttonOffcanvas").click(function () {
+  $("#offcanvasClose").toggleClass("show");
+  // $("#sidebar").toggleClass("hide");
+});
+function openCloseNav(navName, buttonName, backdrop) {
+  $(`#${navName}`).toggleClass("show");
+  $(`#${backdrop}`).toggleClass("show");
+  $(`#${buttonName}`).toggleClass("show");
+}
+function animated(x) {
+  $(x).addClass("animated");
+  // setTimeout(() => {
+  //   $(x).removeClass("animated");
+  // }, 10000);
+}
+function noneAnimated(x) {
+  $(x).removeClass("animated");
+  // setTimeout(() => {
+  //   $(x).removeClass("animated");
+  // }, 10000);
+}
