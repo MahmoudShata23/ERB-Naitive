eaf452db927024c53eb18eb81fc34a7410f1f737
Mahmoud Shata <mahmoudhosam23@gmail.com>
2023-04-01 21:28:36 +0200

    finish profile

--- script.js
+++ script.js
@@ -18,42 +18,47 @@ function openCloseNav(navName, buttonName, backdrop) {
   $(`#${navName}`).toggleClass("show");
   $(`#${backdrop}`).toggleClass("show");
   $(`#${buttonName}`).toggleClass("show");
+  $(".toggle-edit-user").text("Edit");
+  if ($(".toggle-edit-user").text() == "Edit" && !$(`#${navName}`).hasClass("show")) {
+    $(".option-row").removeClass("show");
+  }
 }
 function animated(x) {
   $(x).addClass("animated");
-  // setTimeout(() => {
-  //   $(x).removeClass("animated");
-  // }, 10000);
 }
 function noneAnimated(x) {
   $(x).removeClass("animated");
-  // setTimeout(() => {
-  //   $(x).removeClass("animated");
-  // }, 10000);
 }
 // user profile
-$('.form-show-data').ready(function(){
-  $(".toggle-edit-user").click(function(){
+$(".form-show-data").ready(function () {
+  $(".toggle-edit-user").click(function () {
     $(".form-show-data").toggle(200);
-    $("#editForm").toggle(200)
+    $("#editForm").toggle(200);
+    if ($(".toggle-edit-user").text() == "Edit") {
+      $(".toggle-edit-user").text("Cancel");
+      $(".option-row").addClass("show");
+    } else {
+      $(".toggle-edit-user").text("Edit");
+      $(".option-row").removeClass("show");
+    }
   });
 });
 
-$(document).ready(function() {   
-  var readURL = function(input) {
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-              $('.profile-pic').attr('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-      }
-  }
-  $(".file-upload").on('change', function(){
-      readURL(this);
+$(document).ready(function () {
+  var readURL = function (input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $(".profile-pic").attr("src", e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+  $(".file-upload").on("change", function () {
+    readURL(this);
   });
-  $(".upload-button").on('click', function() {
-     $(".file-upload").click();
+  $(".upload-button").on("click", function () {
+    $(".file-upload").click();
   });
 });
 // end user profile
